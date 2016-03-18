b2dd46d99a650a2bcd23deb21e282fd658adfa70
Elena Pavlovic <lena@pavlovic.ru>
2016-03-18 01:47:16 -0700

    Assignment 3

---
+++ Assignment1/BIT285_assighment1/VisitorsLog.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="VisitorsLog.aspx.cs" Inherits="BIT285_assighment1.VisitorsLog" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Visitor log</title>
+</head>
+<style>
+    li {list-style-type: none;}</style>
+<script   src="https://code.jquery.com/jquery-2.2.2.min.js"   integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI="   crossorigin="anonymous"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+ <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div>
+        <label for="user-list">Visitor table</label>
+        <ul id="user-list">
+
+        </ul>
+    </div>
+    <div id="user-details"></div>
+    </div>
+    </form>
+    <script type="text/javascript" src="js/VisitorLog.js">
+    </script>
+</body>
+</html>
---
+++ Assignment1/BIT285_assighment1/js/VisitorLog.js
@@ -0,0 +1,53 @@
+﻿$(document).ready(function () {
+    var baseUrl = "http://bit285login.azurewebsites.net/api/";
+    // Store an array of users
+    var users = [];
+
+    // Bind users to the list
+    var bindUsers = function (users) {
+        for (i = 0; i < users.length; i++) {
+            var li = $("<li></li>", { "id": users[i].userID, "title": "tooltip", "class": "user-record" });
+            li.html(users[i].firstname + " " + users[i].lastname);
+            li.appendTo("#user-list");
+        }
+    };
+
+    // Make initial call to populate list
+    $.ajax({
+        url: baseUrl + "users",
+        method: "GET"
+    })
+    .done(function (result) {
+        bindUsers(result);
+    });
+
+    // Setup tooltip
+    $(document).tooltip({
+        items: "li",
+        content: function (callback) {
+            var element = $(this);
+          
+            if (element.attr("class") == "user-record") {
+                // Making individual ajax call to get user by their ID
+                $.ajax({
+                    url: baseUrl + "users/" + element.attr("id"),
+                    method: "GET"
+                })
+                .done(function (result) {
+                    var l = $("<ul></ul>");
+                    
+                    $("<li></li>").html("Last login: " + result.last_login).appendTo(l);
+                    $("<li></li>").html("Logged in: " + result.logged_in).appendTo(l);
+                    $("<li></li>").html("IP Address: " + result.ipaddress).appendTo(l);
+
+                    // the result is beenig sent back to the tooltip
+                    callback(l);
+                });
+            }
+            else {
+                // Default value
+                return "Tooltip"
+            }
+        }
+    });
+});
